2ff7021dfeea2790ee1bfd180b6cb710b11016db
Manin Gauba <manindgauba@gmail.com>
2024-04-01 19:40:34 -0400

    added code and api for weather app

---
+++ Client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Main from './components/Main'
+
+
+function App() {
+
+
+  return (
+
+    <div>
+      <Nav />
+      <Main />
+
+
+
+    </div>
+  )
+}
+
+export default App
+
+
+// this is our homepage 

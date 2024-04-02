63e1140d125d82f9856f97648f671b8fc923cbc6
Armond-Guze <Armond.Guze@yahoo.com>
2024-04-01 21:29:34 -0400

    created basic component for navbar, simple styling

--- Client/src/components/Nav.jsx
+++ Client/src/components/Nav.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from 'react';
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
-
-export default Nav
+    <nav className="bg-yellow-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold">Logo</div>
+        <div className="flex items-center">
+          <a href="#" className="text-white mr-4 hover:text-gray-200">Home</a>
+          <a href="#" className="text-white mr-4 hover:text-gray-200">About</a>
+          <a href="#" className="text-white mr-4 hover:text-gray-200">Services</a>
+          <a href="#" className="text-white mr-4 hover:text-gray-200">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nav;

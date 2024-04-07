d2d9805be6e931d48f82eb25c1a3225565a79c27
Armond-Guze <Armond.Guze@yahoo.com>
2024-04-07 12:22:26 -0400

    added engines in json for heroku

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Footer() {
-  const footerStyle = {
-    backgroundColor: '#3b82f6', // Blue color
-    color: 'white',
-    textAlign: 'center',
-    padding: '1rem',
-    position: 'relative',
-    width: '100%',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Shadow effect
-  };
+  const location = useLocation();
 
-  const linkStyle = {
-    color: 'white',
-    textDecoration: 'underline',
-    marginLeft: '0.5rem',
-    marginRight: '0.5rem',
+  const footerStyle = {
+    backgroundColor: "#3b82f6",
+    color: "white",
+    textAlign: "center",
+    padding: "1rem",
+    position: "relative",
+    width: "100%",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    display: location.pathname === "/" ? "block" : "none", // Show only on homepage
   };
 
   return (
     <footer style={footerStyle}>
       <div>
         <span>Follow me on GitHub:</span>
-        <a href="https://github.com/YourGitHubUsername" target="_blank" rel="noopener noreferrer" style={linkStyle}>GitHub</a>
       </div>
     </footer>
   );

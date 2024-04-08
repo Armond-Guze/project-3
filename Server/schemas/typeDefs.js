ac8ea04676849f2c73fad212203f320d30a96441
shumaela101~ <shumu.laeeq@gmail.com>
2024-04-07 23:08:42 -0400

    merge errors

---
+++ Server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const typeDefs = `
+ type User {
+    _id: ID
+    username: String
+    email: String
+    favoriteDestination: [Destination]
+ }
+
+  type Destination {
+    _id: ID
+    name: String
+    location: String
+    topDestination: Boolean
+  }
+  
+  type Auth{
+    token: ID
+    user: User
+  }
+
+  type Query {
+    topTrendingDestinations: [Destination]
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addToFavorites(id: String): User
+    removeFromFavorites(id: ID): User
+  }
+`;
+
+module.exports = typeDefs;

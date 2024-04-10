96b522843bfe5f1d743e6918dcd34dece69bb03a
Armond-Guze <Armond.Guze@yahoo.com>
2024-04-09 23:28:38 -0400

    added commentin login

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { LOGIN } from '../utils/mutations';
-
+// login page
 function LoginPage(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
